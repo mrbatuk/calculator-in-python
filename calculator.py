807d68424b2ef96abf2526ddf518b9636ac41ace
Mehmet Batuk <mehmetbtk@gmail.com>
2022-04-17 15:27:13 +0300

    Added loops in * and **

--- calculator.py
+++ calculator.py
@@ -6,21 +6,28 @@ while True:
     
     #We asked the user to enter an operator
     operator=input("Operator (+,-,/,*,**): ")
-    
+    result=1
+    result2=0
     #check is the operator is correct or not
     if operator == "+" or operator == "-"or operator =="*" or operator =="/" or operator =="**" :
         if operator == "+":
             result=first_number+second_number
+            print(result)
         elif operator =="-":
             result=first_number-second_number
+            print(result)
         elif operator =="*":
-            result=first_number*second_number
+            for x in range(0, second_number):
+                result2=first_number+result2
+            print(result2)
         elif operator =="**":
-            result=first_number**second_number
+            for x in range(0, second_number, 1):
+                result=first_number*result
+            print(result)
         else:
             result=first_number/second_number
         
-        print(result)
+
         
     #error in choosing wrong operator
     else:
